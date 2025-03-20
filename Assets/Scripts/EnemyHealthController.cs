588e8cc6568286bac1a7ff9acc5ad56c16252f9c
Garrett Rogers <grogers1102@gmail.com>
2025-03-20 14:35:52 -0700

    Fixes to TestEnemy prefab

--- Assets/Scripts/EnemyHealthController.cs
+++ Assets/Scripts/EnemyHealthController.cs
@@ -2,18 +2,7 @@ using UnityEngine;
 
 public class EnemyHealthController : MonoBehaviour
 {
-    public float health = 10;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float health = 100;
     public void DamageToHealth(float dmg)
     {
         health -= dmg;
