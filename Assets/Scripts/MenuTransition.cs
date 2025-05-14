e024e3a6ba42b6b915afa118894a6433e677cacc
Garrett Rogers <grogers1102@gmail.com>
2025-05-14 15:26:58 -0700

    Added "Space" input for menu transitions

--- Assets/Scripts/MenuTransition.cs
+++ Assets/Scripts/MenuTransition.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class MenuTransition : MonoBehaviour
 {
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Level1");
+        }
+    }
     public void StartLevel1(){
         UnityEngine.SceneManagement.SceneManager.LoadScene("Level1");
     }
